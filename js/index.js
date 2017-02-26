68f0849914a15ad6bfef7c78669a08af09409920
Dan Dean <dandean@simple.com>
2017-02-25 22:47:32 -0800

    Externalized calculations

--- js/index.js
+++ js/index.js
@@ -1,63 +1,10 @@
 // @flow
-
-// An array of individual costs payed for by U.S. tax payers:
-const baselineCosts: Array<number> = [
-  // mar a logo vacation
-  // http://www.politico.com/story/2017/02/trump-mar-lago-taxpayers-234562
-  // Now three of these:
-  // http://www.cbsnews.com/news/president-trump-mar-a-lago-trips-cost-taxpayers-millions/
-  3000000 * 3,
-  // Eric Trump's Uruguay trip
-  // http://thehill.com/homenews/administration/317863-eric-trump-uruguay-trip-cost-taxpayers-98k-report
-  98000
-];
-
-// All individual costs added to a single value:
-const baseline: number = baselineCosts.reduce((accumulator, value, index) => {
-  return accumulator + value;
-}, 0);
-console.log('baseline', baseline);
-
-// Cost for security in NYC
-// http://money.cnn.com/2016/11/21/news/protecting-donald-trump/
-const nycCostPerYear: number = 1000000.00 * 365;
-console.log('nycCostPerYear', nycCostPerYear);
-
-const msPerYear: number = 31536000000;
-console.log('msPerYear', msPerYear);
-
-const nycCostPerMs: number = nycCostPerYear / msPerYear;
-console.log('nycCostPerMs', nycCostPerMs);
-
-const trumpStartDate: Date = new Date('Jan 20 2017 12:00:00 EST');
-console.log('trumpStartDate', trumpStartDate);
-
-const trumpStartMs: number = trumpStartDate.getTime();
-console.log('trumpStartMs', trumpStartMs);
-
-const valueAtBoot: number = calculate();
-console.log('valueAtBoot', valueAtBoot);
-
-const formattedValueAtBoot = format(valueAtBoot);
-console.info('formattedValueAtBoot', formattedValueAtBoot);
+import { calculate, usdollars } from 'trumptax-calculator';
 
 const container: ?HTMLElement = document.querySelector('.dollars');
 
-function calculate(): number {
-  const elapsedTime: number = Date.now() - trumpStartMs;
-  return (elapsedTime * nycCostPerMs) + baseline;
-}
-
-function format(value: number): string {
-  const formatted: string = value.toLocaleString('en-US', {
-    style: 'currency',
-    currency: 'USD'
-  });
-  return formatted;
-}
-
 function renderAndQueue(): void {
-  const value: string = format(calculate());
+  const value: string = usdollars(calculate());
 
   if (container) {
     container.innerHTML = value;
